348fc7d9ec51570ea12678171becadaa54113a68
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-05-15 17:24:52 -0700

    Add circlular text path in Overview

--- src/Overview.js
+++ src/Overview.js
@@ -5,6 +5,7 @@ import _ from 'lodash';
 var width = 500;
 var height = 500;
 var innerRadius = 120;
+var innerCircleRadius = 110;
 var fontSize = 24;
 var arcPadding = 5;
 var arc = d3.arc();
@@ -15,6 +16,22 @@ class Overview extends Component {
   componentDidMount() {
     this.svg = d3.select(this.refs.svg)
       .append('g').attr('transform', 'translate(' + [width / 2, height / 2] + ')');
+      
+    // def for text on circular path
+    this.svg
+      .append('defs')
+      .append('path')
+      .attr('id', 'textPath')
+      .attr('d', 'M' + [0, 0] +
+        ' A' + [innerCircleRadius, innerCircleRadius, 0, 1, 1, 0, 0.01])
+      // for some reason SVG path arcs start at 180deg so rotate it 90deg to start at top
+      .attr('transform', 'rotate(90)translate(' + [-innerCircleRadius, 0] + ')');
+    // text path
+    this.svg.append('text')
+      .append('textPath')
+      .attr('xlink:href', '#textPath')
+      .attr('font-size', 1)
+      .text(this.props.sequence);
 
     this.calculateArcs();
     this.renderArcs();
@@ -70,7 +87,6 @@ class Overview extends Component {
       .attr('text-anchor', 'middle')
       .attr('dy', '.35em')
       .text(d => d.data.name);
-
   }
 
   render() {
