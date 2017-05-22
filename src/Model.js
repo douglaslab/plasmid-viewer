156707d7f2e16589383c18878b92ac398c85dc0e
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-05-22 15:45:24 -0700

    Move width/height into Model.js

--- src/Model.js
+++ src/Model.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import * as d3 from 'd3';
 import _ from 'lodash';
 
+var size = window.innerWidth < 500 ? window.innerWidth : 500;
 class Model extends Component {
   render() {
+    var style = {
+      width: size,
+      height: size,
+      display: 'inline-block',
+    };
+
     return (
-      <div width={this.props.width} height={this.props.height} />
+      <div style={style} />
     );
   }
 }
